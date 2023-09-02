ee43d3ad18023dfdbc6922b8640005bc0e565a60
warrenrhodes <webanalyse237@gmail.com>
2023-09-02 14:45:59 +0100

    Update some golden

---
+++ app_customers/test/welcome/welcome_view_goldens_test.dart
@@ -0,0 +1,32 @@
+import 'package:app_customer/data/database/hive_service.dart';
+import 'package:app_customer/data/repository/contactRepository.dart';
+import 'package:app_customer/data/repository/tranfersRepository.dart';
+import 'package:app_customer/pages/welcome/welcome_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:golden_toolkit/golden_toolkit.dart';
+
+import '../golden.dart';
+
+class WelcomeViewWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Get.put(TransferRepository(HiveService(HiveServiceType.fake)));
+    Get.put(ContactRepository(HiveService(HiveServiceType.fake)));
+
+    return WelcomeView();
+  }
+}
+
+void main() {
+  group('Goldens', () {
+    testGoldens('welcome us view', (tester) async {
+      await multiScreenMultiLocaleGolden(
+        tester,
+        WelcomeViewWidget(),
+        'welcome_view',
+      );
+    });
+  });
+}
