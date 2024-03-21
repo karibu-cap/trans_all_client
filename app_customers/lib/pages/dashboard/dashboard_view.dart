3dc47a12b877c469b1bc207d6ac53646d6085ddc
warrenrhodes <webanalyse237@gmail.com>
2024-03-21 21:40:49 +0100

    Update golden

--- app_customers/lib/pages/dashboard/dashboard_view.dart
+++ app_customers/lib/pages/dashboard/dashboard_view.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
