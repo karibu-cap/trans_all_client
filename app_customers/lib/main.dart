c57f001f52155d0e75afcd8e1e09063c4525d4c4
warrenrhodes <webanalyse237@gmail.com>
2024-03-22 21:34:10 +0100

    Adds notification

--- app_customers/lib/main.dart
+++ app_customers/lib/main.dart
@@ -1,11 +1,13 @@
 import 'dart:async';
 
-import 'package:app_customer/util/drawer_controller.dart';
+import 'package:firebase_messaging/firebase_messaging.dart' as fcm;
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:karibu_capital_core_cloud_messaging/cloud_messaging.dart';
+import 'package:karibu_capital_core_cloud_messaging/convert_remote_message.dart';
 import 'package:karibu_capital_core_remote_config/remote_config.dart';
 import 'package:logging/logging.dart';
 import 'package:trans_all_common_config/config.dart';
@@ -19,9 +21,25 @@ import 'data/repository/forfeitRepository.dart';
 import 'data/repository/tranferRepository.dart';
 import 'routes/app_router.dart';
 import 'util/check_transaction.dart';
+import 'util/drawer_controller.dart';
+import 'util/local_notification.dart';
 import 'util/themes.dart';
 import 'util/user_contact.dart';
 import 'widgets/contact_service_button/contact_service_model.dart';
+
+/// Handle the message non app background state.
+@pragma('vm:entry-point')
+Future<void> handlerFcmOnBackgroundMessage(
+  fcm.RemoteMessage remoteMessage,
+) async {
+  final newMessage = convertRemoteMessage(remoteMessage);
+  await LocalNotificationApi.init();
+  await LocalNotificationApi.showNotification(
+    body: newMessage.notification?.body,
+    title: newMessage.notification?.title,
+    payload: newMessage.notification?.title,
+  );
+}
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -33,6 +51,7 @@ Future<void> main() async {
   await AppRouter.init(
     AppRouterType.gorouter,
   );
+  await LocalNotificationApi.init();
 
   await RemoteConfig.init(
     service: getAppConfigDefaults().remoteConfigType,
@@ -63,8 +82,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final hiveService = HiveService(HiveServiceType.hive);
+    final cloudMessaging = CloudMessaging(type: CloudMessagingType.firebase);
     final appThemeData = ThemeManager();
-    final drawerController = CustomDrawerController();
 
     void _initGetProviders() {
       UserContactConfig.init();
@@ -72,6 +91,7 @@ class MyApp extends StatelessWidget {
       Get.lazyPut(() => TransferRepository(hiveService));
       Get.lazyPut(() => ContactRepository(hiveService));
       Get.lazyPut(() => ForfeitRepository(hiveService));
+      Get.lazyPut(() => cloudMessaging);
       Get.lazyPut(() => appThemeData);
       Get.lazyPut(ContactServiceModel.new);
       Get.lazyPut(CustomDrawerController.new);
@@ -79,8 +99,25 @@ class MyApp extends StatelessWidget {
       return;
     }
 
+    Future<void> _initCloudMessaging() async {
+      final CloudMessaging cloudMessaging = Get.find<CloudMessaging>();
+
+      /// Call when the app is in background state.
+      CloudMessaging.onFcmBackgroundMessage(handlerFcmOnBackgroundMessage);
+
+      // Call when the app is in foreground state.
+      cloudMessaging.onMessage?.listen((event) async {
+        await LocalNotificationApi.showNotification(
+          body: event.notification?.body,
+          title: event.notification?.title,
+          payload: event.notification?.title,
+        );
+      });
+    }
+
     _initGetProviders();
     streamPendingTransaction();
+    _initCloudMessaging();
 
     return _BuildApp();
   }
